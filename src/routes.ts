09359403152b0769f7c1d7ba35efcec0871c125c
Alexandrehs <echo.alexandre@gmail.com>
2020-08-05 03:06:35 -0300

    Criado o controller CarParking com o metodo show para mostrar os carros relacionados com a tabela parking, ou seja, lista somente os carros que estao no parking existende. Mudanca np metodo create do controller CarsControlle, agora ele ja faz tambem o relacionamente, ele criar o carro na tabela, e cria o relaciomento entre cars e parking na tabela car_parking

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,13 @@ import cors from 'cors';
 
 import CarsController from './controllers/CarsController';
 import ParkingController from './controllers/ParkingController';
+import CarParkingController from './controllers/CarParkingController';
 
 const routes = Router();
 
 const carsController = new CarsController();
 const parkingController = new ParkingController();
+const carParkingController = new CarParkingController();
 
 routes.post('/cars', cors(), carsController.create);
 routes.post('/cars/:id', cors(), carsController.remove);
@@ -15,5 +17,7 @@ routes.get('/cars', cors(), carsController.show);
 
 routes.post('/parking', cors(), parkingController.create);
 routes.get('/parking', cors(), parkingController.show);
+
+routes.get('/car_parking', cors(), carParkingController.show);
 
 export default routes;
